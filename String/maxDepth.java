40dcecc9dba1541f1be926eb1a4b4a86685db2f3
Sanjeevkoli <53033879+Sanjeevkoli@users.noreply.github.com>
2024-12-05 07:46:55 +0530

    Create maxDepth.java

---
+++ String/maxDepth.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxDepth(String s) {
+        int cnt = 0;
+        int ans = 0;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s.charAt(i) == '(')
+            {
+                cnt++;
+                ans = Math.max(ans, cnt);
+            }
+            else if(s.charAt(i) == ')')
+            {
+                cnt--;
+            }
+        }
+        return ans;
+    }
+}
